eb7e333db1ba3fa065da52dc3f541f7ab74f7243
Andrey Shalin <andrey.shalin@gmail.com>
2026-03-23 18:05:10 +0300

    fix(web): handle favicon request to avoid first-load warning

--- src/main/java/ca/digitalcave/buddi/live/config/WebConfig.java
+++ src/main/java/ca/digitalcave/buddi/live/config/WebConfig.java
@@ -23,6 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.addRedirectViewController("/", "/index");
 		registry.addRedirectViewController("/index.html", "/index");
 		registry.addRedirectViewController("/buddilive", "/index");
+		registry.addRedirectViewController("/favicon.ico", "/img/logo-title-small.png");
 	}
 
 	@Override
