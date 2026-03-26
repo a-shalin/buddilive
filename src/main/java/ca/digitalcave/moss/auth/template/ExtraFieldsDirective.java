10e0fe697fb458c3f939abfc1699ae80347dc236
Andrey Shalin <andrey.shalin@gmail.com>
2026-03-26 11:36:09 +0300

    Migrate index page from FreeMarker to Thymeleaf

--- src/main/java/ca/digitalcave/moss/auth/template/ExtraFieldsDirective.java
+++ src/main/java/ca/digitalcave/moss/auth/template/ExtraFieldsDirective.java
@@ -1,22 +1,8 @@
 package ca.digitalcave.moss.auth.template;
 
-import java.io.IOException;
 import java.io.Writer;
-import java.util.Map;
 import java.util.ResourceBundle;
 
-import freemarker.core.Environment;
-import freemarker.ext.beans.ResourceBundleModel;
-import freemarker.template.TemplateDirectiveBody;
-import freemarker.template.TemplateDirectiveModel;
-import freemarker.template.TemplateException;
-import freemarker.template.TemplateModel;
-
-public abstract class ExtraFieldsDirective implements TemplateDirectiveModel {
-	public void execute(Environment env, @SuppressWarnings("rawtypes") Map params, TemplateModel[] loopVars, TemplateDirectiveBody body) throws TemplateException, IOException {
-		final Writer out = env.getOut();
-		writeFields(out, ((ResourceBundleModel) env.getDataModel().get("i18n")).getBundle());
-	}
-	
+public abstract class ExtraFieldsDirective {
 	public abstract void writeFields(Writer out, ResourceBundle i18n);
 }
