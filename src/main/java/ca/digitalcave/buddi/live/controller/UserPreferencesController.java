9d121ef5afe9809c1cd3c9063539071bcd75ad54
Andrey Shalin <andrey.shalin@gmail.com>
2026-03-28 17:09:39 +0300

    Extract user-preferences writes into transactional service

--- src/main/java/ca/digitalcave/buddi/live/controller/UserPreferencesController.java
+++ src/main/java/ca/digitalcave/buddi/live/controller/UserPreferencesController.java
@@ -4,15 +4,12 @@ import ca.digitalcave.buddi.live.api.converter.UserPreferencesResponseConverter;
 import ca.digitalcave.buddi.live.api.dto.SuccessResponseDto;
 import ca.digitalcave.buddi.live.api.dto.UserPreferencesResponseDto;
 import ca.digitalcave.buddi.live.api.dto.request.UserPreferencesRequestDto;
-import ca.digitalcave.buddi.live.db.*;
-import ca.digitalcave.buddi.live.db.util.ConstraintsChecker;
-import ca.digitalcave.buddi.live.db.util.DataUpdater;
+import ca.digitalcave.buddi.live.service.UserPreferencesTransactionalService;
 import ca.digitalcave.buddi.live.db.util.DatabaseException;
 import ca.digitalcave.buddi.live.model.User;
 import ca.digitalcave.buddi.live.security.CookieUtil;
 import ca.digitalcave.buddi.live.util.LocaleUtil;
 import ca.digitalcave.moss.auth.service.AuthenticationHelper;
-import ca.digitalcave.moss.crypto.Crypto;
 import ca.digitalcave.moss.crypto.Crypto.CryptoException;
 import ca.digitalcave.moss.crypto.DefaultHash;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,34 +17,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Currency;
-import java.util.Locale;
 
 @RestController
 @RequestMapping("/data/userpreferences")
 public class UserPreferencesController {
 
 	@Autowired
-	private Sources sources;
-
-	@Autowired
-	private Transactions transactions;
-
-	@Autowired
-	private ScheduledTransactions scheduledTransactions;
-
-	@Autowired
-	private Entries entries;
-
-	@Autowired
-	private Users users;
-
-	@Autowired
-	private Crypto crypto;
+	private UserPreferencesTransactionalService userPreferencesTransactionalService;
 
 	@Autowired
 	private AuthenticationHelper authenticationHelper;
@@ -61,7 +39,6 @@ public class UserPreferencesController {
 	}
 
 	@PostMapping
-	@Transactional
 	public SuccessResponseDto post(@AuthenticationPrincipal final User user,
 			@RequestBody final UserPreferencesRequestDto dto,
 			final HttpServletRequest request,
@@ -70,57 +47,24 @@ public class UserPreferencesController {
 			final Action action = dto.action();
 
 			if (Action.UPDATE == action) {
-				if (Boolean.TRUE.equals(dto.encrypt()) != user.isEncrypted()) {
+				final boolean toggleEncryption = Boolean.TRUE.equals(dto.encrypt()) != user.isEncrypted();
+				if (toggleEncryption) {
 					final String encryptPassword = dto.encryptPassword();
 					if (!DefaultHash.verify(new String(user.getSecret()), encryptPassword)) {
 						throw new ResponseStatusException(HttpStatus.FORBIDDEN, LocaleUtil.getTranslation(user).getString("INCORRECT_PASSWORD"));
 					}
-
-					if (user.isEncrypted()) {
-						DataUpdater.turnOffEncryption(user, sources, entries, transactions, scheduledTransactions, users, crypto);
-					}
-					else {
-						DataUpdater.turnOnEncryption(user, sources, entries, transactions, scheduledTransactions, users, crypto);
-					}
-				}
-				user.setEmail(Boolean.TRUE.equals(dto.storeEmail()) ? user.getPlaintextIdentifier() : null);
-				user.setLocale(LocaleUtil.parseLocale(dto.locale() != null ? dto.locale() : "en_US", Locale.US));
-				user.setCurrency(Currency.getInstance(dto.currency() != null ? dto.currency() : "USD"));
-				user.setOverrideDateFormat(dto.dateFormat());
-				user.setOverrideCurrencyAfter(Boolean.TRUE.equals(dto.currencyAfter()) || (dto.currencyAfter() == null && user.isCurrencyAfter()) ? "Y" : "N");
-				user.setOverrideDecimalSeparator(dto.decimalSeparator());
-				user.setOverrideThousandsSeparator(dto.thousandSeparator());
-				user.setOverrideNegativeFormat(dto.negativeFormat() != null ? dto.negativeFormat() : "N");
-				user.setShowCurrencySymbol(Boolean.TRUE.equals(dto.showCurrencySymbol()) || (dto.showCurrencySymbol() == null && user.isShowCurrencySymbol()));
-				user.setCurrencySpacing(Boolean.TRUE.equals(dto.currencySpacing()) || (dto.currencySpacing() == null && user.useCurrencySpacing()) ? "Y" : "N");
-				user.setTwoFactorRequired(Boolean.TRUE.equals(dto.useTwoFactor()));
-				user.setShowDeleted(dto.showDeleted() == null || dto.showDeleted());
-
-				ConstraintsChecker.checkUpdateUserPreferences(user);
-
-				final int count = users.updateUser(user);
-				if (count != 1) {
-					throw new DatabaseException(String.format("Update failed; expected 1 row, returned %s", count));
 				}
 
-				if (!user.isTwoFactorRequired()) {
+				final boolean invalidateTwoFactorCookie = userPreferencesTransactionalService.applyAction(user, dto, toggleEncryption);
+				if (invalidateTwoFactorCookie) {
 					CookieUtil.setTwoFactorInvalid(request, response, authenticationHelper);
-					users.updateUserTotpSecret(user, null);
 				}
-
-				DataUpdater.updateBalances(user, sources, transactions, crypto);
 			}
-			else if (Action.INVALIDATE_TOTP_BACKUPS == action) {
-				users.deleteUnusedBackupCodes(user);
-			}
-			else if (Action.DELETE == action) {
-				transactions.deleteAllTransactions(user);
-				scheduledTransactions.deleteAllScheduledTransactions(user);
-				sources.deleteAllSources(user);
-				users.deleteUser(user);
+			else if (Action.INVALIDATE_TOTP_BACKUPS != action && Action.DELETE != action) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, LocaleUtil.getTranslation(user).getString("ACTION_PARAMETER_MUST_BE_SPECIFIED"));
 			}
 			else {
-				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, LocaleUtil.getTranslation(user).getString("ACTION_PARAMETER_MUST_BE_SPECIFIED"));
+				userPreferencesTransactionalService.applyAction(user, dto, false);
 			}
 
 			return new SuccessResponseDto(true);
---
+++ src/main/java/ca/digitalcave/buddi/live/service/UserPreferencesTransactionalService.java
@@ -0,0 +1,100 @@
+package ca.digitalcave.buddi.live.service;
+
+import ca.digitalcave.buddi.live.api.dto.request.UserPreferencesRequestDto;
+import ca.digitalcave.buddi.live.controller.Action;
+import ca.digitalcave.buddi.live.db.Entries;
+import ca.digitalcave.buddi.live.db.ScheduledTransactions;
+import ca.digitalcave.buddi.live.db.Sources;
+import ca.digitalcave.buddi.live.db.Transactions;
+import ca.digitalcave.buddi.live.db.Users;
+import ca.digitalcave.buddi.live.db.util.ConstraintsChecker;
+import ca.digitalcave.buddi.live.db.util.DataUpdater;
+import ca.digitalcave.buddi.live.db.util.DatabaseException;
+import ca.digitalcave.buddi.live.model.User;
+import ca.digitalcave.buddi.live.util.LocaleUtil;
+import ca.digitalcave.moss.crypto.Crypto;
+import ca.digitalcave.moss.crypto.Crypto.CryptoException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Currency;
+import java.util.Locale;
+
+@Service
+public class UserPreferencesTransactionalService {
+
+	private final Sources sources;
+	private final Transactions transactions;
+	private final ScheduledTransactions scheduledTransactions;
+	private final Entries entries;
+	private final Users users;
+	private final Crypto crypto;
+
+	public UserPreferencesTransactionalService(final Sources sources,
+			final Transactions transactions,
+			final ScheduledTransactions scheduledTransactions,
+			final Entries entries,
+			final Users users,
+			final Crypto crypto) {
+		this.sources = sources;
+		this.transactions = transactions;
+		this.scheduledTransactions = scheduledTransactions;
+		this.entries = entries;
+		this.users = users;
+		this.crypto = crypto;
+	}
+
+	@Transactional
+	public boolean applyAction(final User user, final UserPreferencesRequestDto dto, final boolean toggleEncryption) throws CryptoException {
+		final Action action = dto.action();
+		if (Action.UPDATE == action) {
+			if (toggleEncryption) {
+				if (user.isEncrypted()) {
+					DataUpdater.turnOffEncryption(user, sources, entries, transactions, scheduledTransactions, users, crypto);
+				}
+				else {
+					DataUpdater.turnOnEncryption(user, sources, entries, transactions, scheduledTransactions, users, crypto);
+				}
+			}
+			user.setEmail(Boolean.TRUE.equals(dto.storeEmail()) ? user.getPlaintextIdentifier() : null);
+			user.setLocale(LocaleUtil.parseLocale(dto.locale() != null ? dto.locale() : "en_US", Locale.US));
+			user.setCurrency(Currency.getInstance(dto.currency() != null ? dto.currency() : "USD"));
+			user.setOverrideDateFormat(dto.dateFormat());
+			user.setOverrideCurrencyAfter(Boolean.TRUE.equals(dto.currencyAfter()) || (dto.currencyAfter() == null && user.isCurrencyAfter()) ? "Y" : "N");
+			user.setOverrideDecimalSeparator(dto.decimalSeparator());
+			user.setOverrideThousandsSeparator(dto.thousandSeparator());
+			user.setOverrideNegativeFormat(dto.negativeFormat() != null ? dto.negativeFormat() : "N");
+			user.setShowCurrencySymbol(Boolean.TRUE.equals(dto.showCurrencySymbol()) || (dto.showCurrencySymbol() == null && user.isShowCurrencySymbol()));
+			user.setCurrencySpacing(Boolean.TRUE.equals(dto.currencySpacing()) || (dto.currencySpacing() == null && user.useCurrencySpacing()) ? "Y" : "N");
+			user.setTwoFactorRequired(Boolean.TRUE.equals(dto.useTwoFactor()));
+			user.setShowDeleted(dto.showDeleted() == null || dto.showDeleted());
+
+			ConstraintsChecker.checkUpdateUserPreferences(user);
+
+			final int count = users.updateUser(user);
+			if (count != 1) {
+				throw new DatabaseException(String.format("Update failed; expected 1 row, returned %s", count));
+			}
+
+			if (!user.isTwoFactorRequired()) {
+				users.updateUserTotpSecret(user, null);
+			}
+
+			DataUpdater.updateBalances(user, sources, transactions, crypto);
+			return !user.isTwoFactorRequired();
+		}
+		else if (Action.INVALIDATE_TOTP_BACKUPS == action) {
+			users.deleteUnusedBackupCodes(user);
+			return false;
+		}
+		else if (Action.DELETE == action) {
+			transactions.deleteAllTransactions(user);
+			scheduledTransactions.deleteAllScheduledTransactions(user);
+			sources.deleteAllSources(user);
+			users.deleteUser(user);
+			return false;
+		}
+
+		throw new DatabaseException("Unsupported action");
+	}
+}
